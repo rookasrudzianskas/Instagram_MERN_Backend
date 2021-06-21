7a23936a46928d73420d5d94e4fb0c8e215f3831
Rokas <rookas.rudzenskas@gmail.com>
2021-06-21 11:20:22 +0300

    Rokas

--- server.js
+++ server.js
@@ -7,18 +7,21 @@ import mongoose from "mongoose";
 
 
 // app config
+const app = express();
+const port = process.env.PORT || 9002;
 
 // middlewares
-
+app.use(express.json());
+app.use(cors());
 
 // db config
 
 
 // api routes
-
+app.get('/', (req, res) => res.status(200).send('backend is working on 🔥'));
 
 // listener
-
+app.listen(port, () => console.log(`listening on ${port}`));
 
 
 
